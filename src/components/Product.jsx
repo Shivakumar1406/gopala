aed6f81f623e04cef4f513df697a0c8d38b0f7a9
Shivakumar1406 <shivukati14@gmail.com>
2020-02-15 18:04:19 +0530

    first commit

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,6 +3,8 @@ import './product.css';
 export const Product = props=>(
     <div className="product-container">
     <img alt="product" src="ghee.jpg"></img>
+    <button className="bt"><h2>Add to cart</h2></button>
+
     <h1>{props.name}</h1>
     <h2>{props.price}</h2>
     </div>
