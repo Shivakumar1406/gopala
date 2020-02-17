720350d219d374ae8b9f6c48a6608b65a86f0fcc
Shivakumar1406 <shivukati14@gmail.com>
2020-02-17 11:56:24 +0530

    Updates

---
+++ precache-manifest.56f69eb6d4ed63f3d1f20fb08abeac7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3f4f2b043d1ca58fc02c20f8a3fe383",
+    "url": "/Shivakumar1406/gopla.git/index.html"
+  },
+  {
+    "revision": "b1ab262b594d7c433dff",
+    "url": "/Shivakumar1406/gopla.git/static/css/main.4a185628.chunk.css"
+  },
+  {
+    "revision": "00d3254af851e4ff8e59",
+    "url": "/Shivakumar1406/gopla.git/static/js/2.fb10d4da.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Shivakumar1406/gopla.git/static/js/2.fb10d4da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1ab262b594d7c433dff",
+    "url": "/Shivakumar1406/gopla.git/static/js/main.94f9c71e.chunk.js"
+  },
+  {
+    "revision": "1b1b41abc9f8fa9150e8",
+    "url": "/Shivakumar1406/gopla.git/static/js/runtime-main.1dcb2183.js"
+  }
+]);
